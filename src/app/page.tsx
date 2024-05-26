283e3cc70f9c668628dc11efeea07814b655a11b
Pratham <chudasamapratham142@gmail.com>
2024-05-26 19:43:57 +0530

    lenis added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,24 @@
 import Header from "@/components/Header";
 import HeroModel from "@/components/HeroModel";
 import TaglineAndStats from "@/components/TaglineAndStats";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Home() {
 	return (
-		<main className="bg-primary-background">
-			<Header
-			links={[
-				{ href: '/', label: 'STORY' },
-				{ href: '/', label: 'UTILITY' },
-				{ href: '/', label: 'ROADMAP' },
-				{ href: '/', label: 'TEAM' },
-				{ href: '/', label: 'FAQ' },
-			]}
-		/>
-			<HeroModel />
-			<TaglineAndStats />
-		</main>
+		<ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+			<main className="bg-primary-background">
+				<Header
+					links={[
+						{ href: "/", label: "STORY" },
+						{ href: "/", label: "UTILITY" },
+						{ href: "/", label: "ROADMAP" },
+						{ href: "/", label: "TEAM" },
+						{ href: "/", label: "FAQ" },
+					]}
+				/>
+				<HeroModel />
+				<TaglineAndStats />
+			</main>
+		</ReactLenis>
 	);
 }
