f5d06c38f197f2b43aa340d81ccc1d056b2fd151
Pratham <chudasamapratham142@gmail.com>
2024-05-27 10:52:50 +0530

    fixing the path

--- src/stories/LiveCounter.stories.ts
+++ src/stories/LiveCounter.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import Header from "@/stories/Counter";
+import Header from "@/components/Counter";
 
 const meta = {
 	title: "Live/Counter",
